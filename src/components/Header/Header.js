f9762f8de39d63b695e1f02aaabcce499f9619ad
2jacs <anton.nozdrin.21@gmail.com>
2021-03-22 20:59:17 +0300

    Change style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,19 +9,19 @@ export default class Header extends React.Component {
         <header className="header">
            <div className="container">
               <div className="header__inner">
-
+                 <Burger/>
                  {/*Компонент "Пользователь"*/}
-                 <div className="header__user">
-                    <div className="header__user-avatar">
-                       {/*Откуда вытаскивать фотки - public или src?*/}
-                       <img src="img/admin.jpg" alt="" className="header__user-img"/>
-                    </div>
-                    <span className="header__user-name">admin</span>
-                 </div>
+
 
                  <Logo/>
                  {/*Кнопка бургер (добавить меню при нажатии в этот компонент или еще другой)*/}
-                 <Burger/>
+                 <div className="header__user">
+                    <div className="header__user-avatar">
+                       {/*Откуда вытаскивать фотки - public или src?*/}
+                       <img src="admin.jpg" alt="" className="header__user-img"/>
+                    </div>
+                    <span className="header__user-name">admin</span>
+                 </div>
               </div>
            </div>
         </header>
